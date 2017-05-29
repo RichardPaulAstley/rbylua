6bfa8248858302a850fa7cfc39c69c5b129e407c
RichardPaulAstley <bobodu08@hotmail.fr>
2017-05-29 13:44:12 +0200

    Update giftbot.lua
    Updated for Gen 2

--- giftbot.lua
+++ giftbot.lua
@@ -3,30 +3,17 @@ local base_address
 local atkdef
 local spespc
 
-if version == 0xc1a2 or version == 0x36dc or version == 0xd5dd or version == 0x299c then
-	print("RBGY JPN game detected")
-  base_address = 0xd123
-elseif version == 0xe691 or version == 0xa9d then
-	print("Red/Blue USA detected")
-  base_address = 0xd163
-elseif version == 0x7c04 then
-	print("Yellow USA detected")
-  base_address = 0xd162
-elseif version == 0xd289 or version == 0x9c5e or version == 0xdc5c or version == 0xbc2e or version == 0x4a38 or version == 0xd714 or version == 0xfc7a or version == 0xa456 then
-	print("Red/Blue EUR detected")
-  base_address = 0xd168
-elseif version == 0x8f4e or version == 0xfb66 or version == 0x3756 or version == 0xc1b7 then
-	print("Yellow EUR detected")
-  base_address = 0xd167
+if version == 0x9f12 then
+	print("Crystal detected")
+  base_address = 0xdcd7
 else
-	print(string.format("Unknown version, code: %4x", version))
-  print("Script stopped")
+	print(string.format("unknown version, code: %4x", version))
+  print("script stopped")
   return
 end
 
 local size = memory.readbyte(base_address)-1
-local dv_addr = (base_address+0x23)+size*0x2C
-
+local dv_addr = (base_address+0x1D)+size*0x30
  
  
 function shiny(atkdef,spespc)
@@ -57,12 +44,15 @@ while true do
     end
     atkdef = memory.readbyte(dv_addr)
     spespc = memory.readbyte(dv_addr+1)
+    --print(i)
+    print(atkdef)
+    print(spespc)
     if shiny(atkdef,spespc) then
         print("Shiny!!! Script stopped.")
-        print(string.format("Atk: %d", math.floor(atkdef/16)))
-        print(string.format("Def: %d", atkdef%16))
-        print(string.format("Spe: %d", math.floor(spespc/16)))
-        print(string.format("Spc: %d", spespc%16))
+        print(string.format("atk: %d", math.floor(atkdef/16)))
+        print(string.format("def: %d", atkdef%16))
+        print(string.format("spe: %d", math.floor(spespc/16)))
+        print(string.format("spe: %d", spespc%16))
         savestate.save(state)
         break
     else
